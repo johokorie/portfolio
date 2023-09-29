7f6690b93aef80a532eeec57b3a6df4d8b6031b8
John Okorie <ameachijohn001@gmail.com>
2023-09-29 23:05:35 +0100

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,198 @@
+const logotext = "{ / JOHN / }";
+const meta = {
+	title: "John",
+	description:
+		"I'm John aka Panda, a self-taught Frontend developer and technology enthuist.",
+};
+
+const introdata = {
+	title: "Hi 👋, I'm John",
+	animated: {
+		first: "Frontend Developer",
+		second: "Visionary",
+		third: "Problem-solver",
+		fourth: "Programmer",
+		fifth: "Curious",
+		sixth: "Technologist",
+		// add more if you'd like but make sure to update /src/pages/home/index.js Line 29
+	},
+	description:
+		"A self-taught and passionate developer with a flair for creating dynamic, user-friendly interfaces.",
+	your_img_url: "https://i.ibb.co/wRjcnGL/me.jpg",
+};
+
+const dataabout = {
+    title: "BIO",
+    aboutme: "My name is John, I am a Frontend developer that is proficient in building responsive web applications  that marry seamless functionality with striking design. In all of my endeavour, I am so committed in delivering engaging user experiences that satisfy the users that I build for.",
+};
+const worktimeline = [
+	{
+		jobtitle: "Opensource Contributor",
+		where: "Resume Matcher",
+		date: "Present",
+	},
+	{
+		jobtitle: "Frontend Engineer",
+		where: "Genesys Tech Hub",
+		date: "2023",
+	},
+	{
+		jobtitle: "Dev-guild Contributor",
+		where: "Bankless DAO",
+		date: "2022",
+	},
+	{
+		jobtitle: "IT Support",
+		where: "Evergreen School",
+		date: "2021",
+	},
+];
+
+const skills = [
+	{
+		name: "JavaScript",
+	},
+
+	{
+		name: "React",
+	},
+
+	{
+		name: "Git",
+	},
+
+	{
+		name: "Bash",
+	},
+
+	{
+		name: "Nextjs",
+	},
+
+	{
+		name: "HTML",
+	},
+
+	{
+		name: "CSS",
+	},
+
+	{
+		name: "Material UI",
+	},
+
+	{
+		name: "Shell",
+	},
+	{
+		name: "Design Thinking",
+	},
+	{
+		name: "Agile Scrum",
+	},
+	{
+		name: "SEO/SEM",
+	},
+	{
+		name: "Digital Marketing",
+	},
+	{
+		name: "Netlify",
+	},
+	{
+		name: "Vercel",
+	},
+	{
+		name: "GCP",
+	},
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ameachijohn01@gmail.com",
+    description: "Looking forward to hearing from you!",
+    // creat an emailjs.com account
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "SERVICE_ID",
+    YOUR_TEMPLATE_ID: "TEMPLATE_ID",
+    YOUR_USER_ID: "USER_ID (PUBLIC_KEY)",
+};
+
+const socialprofils = {
+    github: "https://github.com/helloojohn",
+    linkedin: "https://linkedin.com/okoriejohn",
+    twitter: "https://twitter.com/"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
